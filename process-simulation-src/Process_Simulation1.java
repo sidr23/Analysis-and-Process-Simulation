3eabf9c4460902cefc9d0f66189840bd3b25c3b4
Siddharth Ramanathan <ramanathan.s@husky.neu.edu>
2018-08-11 23:37:31 -0400

    Update Process_Simulation1.java

--- process-simulation-src/Process_Simulation1.java
+++ process-simulation-src/Process_Simulation1.java
@@ -26,13 +26,6 @@ public class Process_Simulation1 extends javax.swing.JFrame
     initComponents();
   }
   
-
-
-
-
-
-
-
   private void initComponents()
   {
     jPanel1 = new javax.swing.JPanel();
@@ -92,102 +85,20 @@ public class Process_Simulation1 extends javax.swing.JFrame
     });
     GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
     jPanel1.setLayout(jPanel1Layout);
-    jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addContainerGap().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(jScrollPane1).addGroup(jPanel1Layout.createSequentialGroup().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING).addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING).addComponent(jLabel1).addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false).addComponent(Back, javax.swing.GroupLayout.Alignment.LEADING, -1, -1, 32767).addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, -1, -1, 32767))).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addGap(19, 19, 19).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING).addComponent(iv, -2, 81, -2).addComponent(ih, -2, 81, -2)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(jLabel4)).addGroup(jPanel1Layout.createSequentialGroup().addComponent(height, -2, 81, -2).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(jLabel5))).addGap(0, 0, 32767)).addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup().addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 6, 32767).addComponent(Image).addGap(18, 18, 18).addComponent(Enter, -2, 72, -2))))).addContainerGap()));
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addContainerGap().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel1).addComponent(iv, -2, -1, -2)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel2).addComponent(ih, -2, -1, -2).addComponent(jLabel4)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel3).addComponent(height, -2, -1, -2).addComponent(jLabel5)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(Enter).addComponent(Back).addComponent(Image)).addGap(18, 18, 18).addComponent(jScrollPane1, -1, 227, 32767).addContainerGap()));
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addContainerGap().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(jScrollPane1).addGroup(jPanel1Layout.createSequentialGroup().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING).addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING).addComponent(jLabel1).addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false).addComponent(Back, javax.swing.GroupLayout.Alignment.LEADING, -1, -1, 32767).addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, -1, -1, 32767))).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addGap(19, 19, 19).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING).addComponent(iv, -2, 81, -2).addComponent(ih, -2, 81, -2)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(jLabel4)).addGroup(jPanel1Layout.createSequentialGroup().addComponent(height, -2, 81, -2).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(jLabel5))).addGap(0, 0, 32767)).addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup().addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 6, 32767).addComponent(Image).addGap(18, 18, 18).addComponent(Enter, -2, 72, -2))))).addContainerGap()));    
+    jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(jPanel1Layout.createSequentialGroup().addContainerGap().addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel1).addComponent(iv, -2, -1, -2)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel2).addComponent(ih, -2, -1, -2).addComponent(jLabel4)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel3).addComponent(height, -2, -1, -2).addComponent(jLabel5)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED).addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(Enter).addComponent(Back).addComponent(Image)).addGap(18, 18, 18).addComponent(jScrollPane1, -1, 227, 32767).addContainerGap()));    
     p2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Output", 0, 0, null, new java.awt.Color(0, 0, 204)));
     
     GroupLayout p2Layout = new GroupLayout(p2);
     p2.setLayout(p2Layout);
-    p2Layout.setHorizontalGroup(p2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 380, 32767));
-    
-
-
+    p2Layout.setHorizontalGroup(p2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 380, 32767));    
     p2Layout.setVerticalGroup(p2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 0, 32767));
     
-
-
-
     GroupLayout layout = new GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
-    layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap().addComponent(jPanel1, -2, -1, -2).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(p2, -1, -1, 32767).addContainerGap()));
-    
-
-
-
-
-
-
-
+    layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap().addComponent(jPanel1, -2, -1, -2).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(p2, -1, -1, 32767).addContainerGap()));    
     layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap().addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(p2, -1, -1, 32767).addComponent(jPanel1, -1, -1, 32767)).addContainerGap()));
     
-
-
-
-
-
-
-
-
     pack();
   }
   
@@ -265,13 +176,6 @@ public class Process_Simulation1 extends javax.swing.JFrame
     GFTank gf = new GFTank();
     gf.setVisible(true);
   }
-  
-
-
-
-
-
-
   public static void main(String[] args)
   {
     try
@@ -290,10 +194,7 @@ public class Process_Simulation1 extends javax.swing.JFrame
       java.util.logging.Logger.getLogger(Process_Simulation1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
     } catch (javax.swing.UnsupportedLookAndFeelException ex) {
       java.util.logging.Logger.getLogger(Process_Simulation1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    
-
-
+    }    
     java.awt.EventQueue.invokeLater(new Runnable() {
       public void run() {
         new Process_Simulation1().setVisible(true);
